c1c123fbb4494f5a54183afc5dbafa7fcf49a337
Miroslav Holubec <miroslav.holubec@ishisystems.com>
2017-11-24 14:31:27 +0100

    NiFi fetcher reloaded - connection can be completely reset by using close() and ensure() - commit of NiFi transaction only when all data packets were consumed - not consuming all data twice (inside hasNext() and next())

--- indexr-server/src/main/java/io/indexr/server/rt/fetcher/NifiFetcher.java
+++ indexr-server/src/main/java/io/indexr/server/rt/fetcher/NifiFetcher.java
@@ -1,48 +1,37 @@
 package io.indexr.server.rt.fetcher;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.nifi.remote.Transaction;
+import org.apache.nifi.remote.Transaction.TransactionState;
+import org.apache.nifi.remote.TransferDirection;
+import org.apache.nifi.remote.client.SiteToSiteClient;
+import org.apache.nifi.remote.protocol.DataPacket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.indexr.segment.SegmentSchema;
 import io.indexr.segment.rt.Fetcher;
 import io.indexr.segment.rt.UTF8JsonRowCreator;
 import io.indexr.segment.rt.UTF8Row;
 import io.indexr.util.JsonUtil;
-import io.indexr.util.UTF8Util;
-import kafka.consumer.ConsumerIterator;
-import org.apache.nifi.remote.Transaction;
-import org.apache.nifi.remote.TransferDirection;
-import org.apache.nifi.remote.client.SiteToSiteClient;
-import org.apache.nifi.remote.protocol.DataPacket;
-import org.apache.nifi.stream.io.StreamUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
 
 public class NifiFetcher implements Fetcher {
-    private static final Logger logger = LoggerFactory.getLogger(NifiFetcher.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(NifiFetcher.class);
 
-    private SiteToSiteClient siteToSiteClient;
-
-    private Transaction transaction;
-
+    private final SiteToSiteClient.Builder siteToSiteClientBuilder;
     private final UTF8JsonRowCreator utf8JsonRowCreator;
 
-    private Iterator<byte[]> eventItr;
-
-    private volatile List<byte[]> remaining;
-
-    private boolean hasData;
-
-    private boolean hasNextShouldReturnFalse;
-
-    private SegmentSchema tableSchema;
-
-    private List<byte[]> dataList = new ArrayList<>();
-
+    private SiteToSiteClient siteToSiteClient;
+    private Transaction transaction;
+    private DataPacket dataPacket;
 
     @JsonProperty("number.empty.as.zero")
     public boolean numberEmptyAsZero;
@@ -52,24 +41,16 @@ public class NifiFetcher implements Fetcher {
 
     @JsonCreator
     public NifiFetcher(@JsonProperty("number.empty.as.zero") Boolean numberEmptyAsZero,
-                       @JsonProperty("properties") Properties properties) {
-
+            @JsonProperty("properties") Properties properties) {
         this.properties = properties;
-
-//        logger.info("properties {}",properties);
         String urlString = properties.getProperty("nifi.connection.url");
         String portName = properties.getProperty("nifi.connection.portName");
         String requestBatchCount = properties.getProperty("nifi.connection.requestBatchCount");
-        SiteToSiteClient.Builder builder = new SiteToSiteClient.Builder()
-                .url(urlString)
-                .portName(portName)
-                .requestBatchCount(Integer.valueOf(requestBatchCount));
-
+        this.siteToSiteClientBuilder = new SiteToSiteClient.Builder();
+        this.siteToSiteClientBuilder.url(urlString);
+        this.siteToSiteClientBuilder.portName(portName);
+        this.siteToSiteClientBuilder.requestBatchCount(Integer.valueOf(requestBatchCount));
         this.utf8JsonRowCreator = new UTF8JsonRowCreator(this.numberEmptyAsZero);
-        siteToSiteClient = builder.build();
-//        logger.info("siteToSiteClient {}", siteToSiteClient);
-        hasData = false;
-        hasNextShouldReturnFalse = false;
     }
 
     @Override
@@ -78,92 +59,114 @@ public class NifiFetcher implements Fetcher {
     }
 
     @Override
-    public boolean ensure(SegmentSchema schema) throws Exception {
-//        logger.info("ensure");
-        this.tableSchema = schema;
-        return true;
-    }
-
-    @Override
-    public boolean hasNext() throws Exception {
-        if (hasNextShouldReturnFalse) {
-            return false;
-        }
-        transaction = siteToSiteClient.createTransaction(TransferDirection.RECEIVE);
-        DataPacket dataPacket = transaction.receive();
-        if (dataPacket == null) {
-            return false;
-        }
-        transaction.cancel("hasNext");
-//        logger.info("hasNext returning true");
-        return true;
-    }
-
-    @Override
-    public List<UTF8Row> next() throws Exception {
-
-        transaction = siteToSiteClient.createTransaction(TransferDirection.RECEIVE);
-        DataPacket dataPacket;
-        List<UTF8Row> utf8RowsToReturn = new ArrayList<>();
-        while (true) {
-            dataPacket = transaction.receive();
-            if (dataPacket == null) {
-                hasNextShouldReturnFalse = true;
-                break;
+    public synchronized boolean ensure(SegmentSchema schema) {
+        if (transaction != null) {
+            try {
+                TransactionState state = transaction.getState();
+                if (state == TransactionState.TRANSACTION_STARTED || state == TransactionState.DATA_EXCHANGED
+                        || state == TransactionState.TRANSACTION_CONFIRMED) {
+                    return hasNext();
+                } else {
+                    transaction = null;
+                    dataPacket = null;
+                }
+            } catch (IOException e) {
+                LOGGER.error("Cannot get state of the transaction, closing client", e);
+                IOUtils.closeQuietly(siteToSiteClient);
+                siteToSiteClient = null;
+                transaction = null;
+                dataPacket = null;
             }
-
-            final InputStream in = dataPacket.getData();
-            final long size = dataPacket.getSize();
-            final byte[] buff = new byte[(int) size];
-
-            StreamUtils.fillBuffer(in, buff);
-//            logger.info("buff {}", new String(buff));
-            List<UTF8Row> utf8Rows = parseUTF8Row(buff);
-            utf8RowsToReturn.addAll(utf8Rows);
-//            logger.info("utf8Rows {}", utf8Rows.size());
         }
-//        logger.info("utf8RowsToReturn size {}", utf8RowsToReturn.size());
-        return utf8RowsToReturn;
+        if (siteToSiteClient == null) {
+            siteToSiteClient = siteToSiteClientBuilder.build();
+        }
+        try {
+            transaction = siteToSiteClient.createTransaction(TransferDirection.RECEIVE);
+            return hasNext();
+        } catch (Exception e) {
+            LOGGER.error("Cannot create or read transaction", e);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean hasNext() throws IOException {
+        if (this.dataPacket == null && this.transaction != null) {
+            this.dataPacket = this.transaction.receive();
+        }
+        return this.dataPacket != null;
+    }
+
+    @Override
+    public List<UTF8Row> next() throws IOException {
+        DataPacket packet = dataPacket;
+        dataPacket = null;
+        return parseUTF8Row(readDataPacket(packet));
+    }
+
+    private byte[] readDataPacket(DataPacket packet) throws IOException {
+        return IOUtils.toByteArray(packet.getData(), packet.getSize()); // we cannot close the stream
     }
 
     private List<UTF8Row> parseUTF8Row(byte[] data) {
         try {
             return utf8JsonRowCreator.create(data);
         } catch (Exception e) {
-            if (logger.isDebugEnabled()) {
-                logger.debug("Illegal data: {} ", UTF8Util.fromUtf8(data), e);
-            }
+            LOGGER.debug("Illegal data", e);
             return Collections.emptyList();
         }
     }
 
     @Override
     public synchronized void close() throws IOException {
-//        logger.info("nifi close");
-        siteToSiteClient.close();
+        if (this.transaction != null) {
+            try {
+                this.transaction.cancel("Closing the Fetcher");
+            } catch (Exception e) {
+                LOGGER.error("Cannot cancel transaction", e);
+            }
+            this.transaction = null;
+            this.dataPacket = null;
+        }
+        IOUtils.closeQuietly(this.siteToSiteClient);
+        this.siteToSiteClient = null;
     }
 
     @Override
     public void commit() {
-//        logger.info("nifi commit");
+        // commit only in case we have fully read the transaction
         try {
-            transaction.confirm();
-            transaction.complete();
-            hasNextShouldReturnFalse = false;
+            if (!hasNext()) {
+                try {
+                    transaction.confirm();
+                    transaction.complete();
+                } catch (IOException e) {
+                    LOGGER.error("Cannot commit transaction", e);
+                }
+                transaction = null;
+                dataPacket = null;
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.warn("Cannot read from transaction", e);
         }
     }
 
     @Override
     public String toString() {
         String settings = JsonUtil.toJson(this);
-        return String.format("Kafka 0.8 fetcher: %s", settings);
+        return String.format("Nifi fetcher: %s", settings);
     }
 
     @Override
     public boolean equals(Fetcher o) {
-        return super.equals(o);
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        NifiFetcher that = (NifiFetcher) o;
+        return properties != null ? properties.equals(that.properties) : that.properties == null;
     }
 
     @Override
@@ -189,19 +192,5 @@ public class NifiFetcher implements Fetcher {
     @Override
     public void statReset() {
         utf8JsonRowCreator.resetStat();
-    }
-
-    private Iterator<byte[]> toItr(ConsumerIterator<byte[], byte[]> consumerIterator) {
-        return new Iterator<byte[]>() {
-            @Override
-            public boolean hasNext() {
-                return consumerIterator.hasNext();
-            }
-
-            @Override
-            public byte[] next() {
-                return consumerIterator.next().message();
-            }
-        };
     }
 }
